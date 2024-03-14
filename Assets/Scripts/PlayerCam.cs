33067aa529fa14748c4f2378e81b0340cf7619a9
ouijajuice <eearls@ltu.edu>
2024-03-14 01:49:20 -0400

    beningings
    changed:
    -scenery in pit
    -jumping
    -lighting

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -8,7 +8,7 @@ public class PlayerCam : MonoBehaviour
     public Transform playerBody;
 
     float xRotation;
-    float yRotation;
+    //float yRotation;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     public Rigidbody player;
     public float speed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public float groundCheckDistance;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +24,17 @@ public class PlayerMove : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
+        bool grounded = Physics.Raycast(groundCheck.position, Vector3.down, groundCheckDistance);
 
+        bool jumping = Input.GetButtonDown("Jump");
 
         Vector3 move = transform.right * x +transform.forward * z;
+
+        if (jumping && grounded)
+        {
+            player.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+        }
 
         player.position += move * speed * Time.deltaTime;
     }
